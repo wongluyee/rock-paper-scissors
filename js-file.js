343e93270202b2d2c364efb580d314920612d712
Luyee Wong <111058709+wongluyee@users.noreply.github.com>
2022-12-27 11:08:31 +0900

    Added comments to note down my mistakes

--- js-file.js
+++ js-file.js
@@ -3,6 +3,9 @@ function getComputerChoice(){
     return items[Math.floor(Math.random()*items.length)];
 }
 
+/*Define player score and computer score outside the function,
+so that both playRound() function and game() function can access it.
+*/
 let playerScore = 0;
 let computerScore = 0;
 
@@ -10,7 +13,11 @@ function playRound (playerSelection, computerSelection){
   const win = `You win! ${playerSelection} beats ${computerSelection}.`;
   const lose = `You lose! ${computerSelection} beats ${playerSelection}.`;
   const draw = `It's a draw! Both of you selected ${playerSelection}.`;
-
+  
+  /*
+  return statement need to be the last, if not it will stop executing the function,
+  and score wouldn't be added, result text wouldn't be shown.
+  */
   if (playerSelection == computerSelection) {
         return draw;
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') || 
@@ -32,8 +39,14 @@ function playRound (playerSelection, computerSelection){
 
 function game(){
     for (let i = 0; i < 5; i++) {
+        /*Define player selection and computer selection inside the function
+        so that they can be looped also, if not JS will keep using the 1st entry that player entered.
+        */
         const playerSelection = prompt('Choose from rock, paper, or scissors.').toLowerCase();
         const computerSelection = getComputerChoice();
+        /*Need to include the parameters, 
+        if not player selection and computer selection will become undefined.
+        */
         console.log(playRound(playerSelection, computerSelection));
     }
     if (playerScore > computerScore) {
@@ -45,4 +58,5 @@ function game(){
     }
 }
 
+//If written as console.log(game());, it will return undefined in the last row of console.
 game();
