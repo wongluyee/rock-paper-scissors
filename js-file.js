aa76b96ad808bbd51f11ba9266eef07e7444c414
luyeewong <wongluyee1015@hotmail.com>
2023-01-01 20:43:09 +0900

    Added an event listener to call playRound when button is clicked

--- js-file.js
+++ js-file.js
@@ -1,37 +1,54 @@
 const buttons = document.querySelectorAll('button');
+const score = document.querySelector('.score');
+const result = document.querySelector('.result');
 
-let items = ['rock','paper','scissors'];
 function getComputerChoice(){
+    let items = ['rock','paper','scissors'];
     return items[Math.floor(Math.random()*items.length)];
 }
 
 let playerScore = 0;
 let computerScore = 0;
 
-let computerSelection = getComputerChoice();
-let playerSelection = buttons.forEach(button =>{
+buttons.forEach(button =>{
     button.addEventListener('click', function(){
-        playerSelection = button.value;
-        console.log(playRound(playerSelection, computerSelection));
+        playRound(button.value);
     })
-});
+})
 
-function playRound (playerSelection, computerSelection){
-  const win = `You win! ${playerSelection} beats ${computerSelection}.`;
-  const lose = `You lose! ${computerSelection} beats ${playerSelection}.`;
-  const draw = `It's a draw! Both of you selected ${playerSelection}.`;
- 
-  if (playerSelection == computerSelection) {
-        return draw;
+function playRound (playerSelection){
+    let computerSelection = getComputerChoice();
+    
+    if (playerSelection == computerSelection) {
+        score.textContent = `It's a draw! Both of you selected ${playerSelection}.
+        Your score: ${playerScore}, Computer's score: ${computerScore}`;
     } else if ((playerSelection == 'rock' && computerSelection == 'paper') || 
     (playerSelection == 'paper' && computerSelection == 'scissors') || 
     (playerSelection == 'scissors' && computerSelection == 'rock')){
         computerScore+=1;
-        return lose;
+        score.textContent = `You lose, ${computerSelection} beats ${playerSelection}.
+        Your score: ${playerScore}, Computer's score: ${computerScore}`;
+
+        if (computerScore === 5) {
+            result.textContent = "Sorry, you lose...Reload the page to try again."
+            disableButtons();
+        }
     } else if((playerSelection == 'rock' && computerSelection == 'scissors') || 
     (playerSelection == 'paper' && computerSelection == 'rock')||
     (playerSelection == 'scissors' && computerSelection == 'paper')){
         playerScore+=1;
-        return win;
+        score.textContent = `You win, ${playerSelection} beats ${computerSelection}. 
+        Your score: ${playerScore}, Computer's score: ${computerScore}`;
+
+        if (playerScore === 5) {
+            result.textContent = "Congratulations! You win! Reload the page to play again."
+            disableButtons();
+        }
     } 
+}
+
+function disableButtons (){
+    buttons.forEach(btn => {
+        btn.disabled = true;
+    })
 }
